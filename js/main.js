d67ab8410e0696e147550508fe96f9df01ffcd61
Marcos <marcosyano@live.com.pt>
2022-03-14 17:39:19 +0900

    Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas.nome);
+alert(frutas.cor);
+
+
+
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça" , "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+/*
 var nome = "Marcos";
 var n1 = 5
 var n2 = 3;
@@ -8,3 +33,4 @@ console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toL); 
 //alert(frase.replace("Japão", "Brasil")); // Aqui esta sendo substituido o Japão pelo Brasil.
+*/
