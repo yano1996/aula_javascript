3bb821ab41554e4757f719d9f9292e24993568c7
Marcos <marcosyano@live.com.pt>
2022-03-14 23:18:12 +0900

    Parte 2: Manipulandoelementos da pagina

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b> ";
+   // console.log(document.getElementById("agradecimento"));
+   // alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    //window.open("https://www.udemy.com/");
+    window.location.href = "https://www.udemy.com/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML= "Obrigado por passar o mouse";
+    elemento.innerHTML= "Obrigado por passar o mouse"
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML= "Passe o mouse aqui";}
+    elemento.innerHTML= "Passe o mouse aqui"
+}
+
+function load(){
+    alert("Pagina carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*
 function soma(n1,n2){
     return n1+n2;
 }
-var validar = 0 ;
+*/
 
+/*
+var validar = 0 ;
 function validaIdade(idade){
     if(idade >= 18){
       validar = true;
@@ -11,8 +44,9 @@ function validaIdade(idade){
     }
     return validar;
 }
-var idade = prompt("Qual sua idade");
-console.log(validaIdade(idade));
+*/
+//var idade = prompt("Qual sua idade");
+//console.log(validaIdade(idade));
 
 //alert(soma(5,10));
 
