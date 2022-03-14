e3323e18d471e4f452e41ad9aa0b26e0b14b20cd
Marcos <marcosyano@live.com.pt>
2022-03-14 20:38:43 +0900

    Desenvolva paginas web com javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+function soma(n1,n2){
+    return n1+n2;
+}
+var validar = 0 ;
+
+function validaIdade(idade){
+    if(idade >= 18){
+      validar = true;
+    }else{
+        validar=false
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+
+//alert(soma(5,10));
+
+
+
+/*
 var d = new Date();
 alert(d);
 alert(d.getDate());
 alert(d.getHours());
 alert(d.getMinutes());
+*/
+
 
 /*
 var count;
