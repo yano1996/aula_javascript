b013948c7f950fb871aded5bf7c72ddafda3ed24
Marcos <marcosyano@live.com.pt>
2022-03-14 17:18:13 +0900

    JavaScript basico

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Marcos";
+var n1 = 5
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo"
+//alert( nome +" tem " + idade + "  anos");
+//alert (idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toL); 
+//alert(frase.replace("Japão", "Brasil")); // Aqui esta sendo substituido o Japão pelo Brasil.
